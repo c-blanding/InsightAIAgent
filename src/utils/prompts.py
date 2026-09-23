cc45220ead7723eb7cc279b052a677714a457e2f
c-blanding <b.blanding90@yahoo.com>
2026-09-22 23:04:31 -0400

    Added plan_instruction_prompt

---
+++ src/utils/prompts.py
@@ -0,0 +1,9 @@
+plan_intruction_prompt = """
+You are a QA and Debugging assistant.
+You will receive a URL
+{url}
+You will receive a description of the bug
+{bug}
+Your job is to return a step by step plan to find this problem
+"""
+
